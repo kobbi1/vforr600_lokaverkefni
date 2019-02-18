7127e211929a124de15473d112ac8f69d5f26780
kobbi1 <jakob-daniel@hotmail.com>
2019-02-18 14:15:19 +0000

    ParticipateInForumTest Added - Vid 4

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{$thread->title}}</div>
+                <div class="card-header">
+                    <a href="#">{{$thread->creator->name }}</a> posted:
+                    {{$thread->title}}
+                </div>
 
                 <div class="card-body">
                     <div class="body">{{ $thread->body }}</div>
@@ -17,17 +20,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @foreach($thread->replies as $reply)
-            <div class="card mt-4">
-            <div class="card-header">
-                <a href="#">
-                    {{$reply->owner->name}}
-                </a>
-                said {{$reply->created_at->diffForHumans() }}...
-            </div>
-                <div class="card-body">
-                    <div class="body">{{ $reply->body }}</div>
-                </div>
-            </div>
+                @include("threads.reply")
             @endforeach
         </div>
     </div>
