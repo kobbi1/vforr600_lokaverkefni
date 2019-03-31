be71ca2df285c4ecc9ae8007205cc1d187daf949
Jakob Daniel <jakob-daniel@hotmail.com>
2019-03-31 15:42:44 +0000

    A User can delete a reply vid 31 Done

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -22,4 +22,15 @@
     <div class="card-body">
         <div class="body">{{ $reply->body }}</div>
     </div>
+
+    @can ("update", $reply)
+        <div class="card-footer">
+            <form method="POST" action="/replies/{{ $reply->id }}">
+                {{ csrf_field() }}
+                {{ method_field("DELETE") }}
+
+                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+            </form>
+        </div>
+    @endcan
 </div>
