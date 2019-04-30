5c890f9e67fd9620c4df339b266f52fb378ca3ed
kobbi1 <jakob-daniel@hotmail.com>
2019-04-30 13:17:03 +0000

    Pisli

--- public/js/app.js
+++ public/js/app.js
@@ -49944,8 +49944,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\VFORR600\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\VFORR600\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! H:\VFORR600\lokaverkefni\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! H:\VFORR600\lokaverkefni\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
